b84d5b15080b2ec1afd614cda76ad0f1aff54c2c
Deniz Sarıkaş <75947870+denizsarikas@users.noreply.github.com>
2023-01-27 14:35:53 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/create" element={<Create />}></Route>
+            <Route path="/blogs/:id" element={<BlogDetails />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
+//npx json-server --watch data/db.json --port 8000
